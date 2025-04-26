4359f502b06772bc76771c955dc12c5a9a364bbb
Miles Zimmerman <miles@dealops.com>
2025-04-26 16:25:09 -0700

    fix deploy and add retell stuff

--- run_web_app.py
+++ run_web_app.py
@@ -8,4 +8,4 @@ from schedulur.web_app import app
 
 if __name__ == "__main__":
     print("Starting Schedulur web app on http://127.0.0.1:5000")
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=5000)
