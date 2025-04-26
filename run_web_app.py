5c35431af3ca3ceb4f49c1bb5f10df8b67de85e4
Miles Zimmerman <miles@dealops.com>
2025-04-26 16:30:30 -0700

    add dotenv

--- run_web_app.py
+++ run_web_app.py
@@ -4,8 +4,15 @@
 Run script for Schedulur web app
 """
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 from schedulur.web_app import app
+
+# if HOST is not set, use default
+HOST = os.getenv("HOST", "0.0.0.0")
 
 if __name__ == "__main__":
     print("Starting Schedulur web app on http://127.0.0.1:5000")
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    app.run(debug=True,host=HOST,port=5000)
